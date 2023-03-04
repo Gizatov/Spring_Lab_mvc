98a017f02ec249056003e6e5fa157ae78a63c832
Gizatov <92961346+Gizatov@users.noreply.github.com>
2023-03-05 01:48:07 +0600

    Labwork-2
    Done

---
+++ LabSpring/src/main/java/kz/arnur/gizatov/HelloController.java
@@ -0,0 +1,13 @@
+package kz.arnur.gizatov;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello-world")
+    public String sayhello(){
+        return "hello_world";
+
+    }
+}
